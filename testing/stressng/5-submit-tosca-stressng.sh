3c2443e3c93725dc4a4b7ab0c9cde8ca93c6ff53
jaydesl <jay.deslauriers@gmail.com>
2018-05-18 14:17:31 +0100

    Add submitter API helper scripts

---
+++ testing/stressng/5-submit-tosca-stressng.sh
@@ -0,0 +1,14 @@
+
+if [ -z "$MICADO_MASTER" ]; then
+    if [[ $# -eq 0 ]] ; then
+       echo 'Please, specify the ip address or set MICADO_MASTER!'
+       exit 1
+    fi
+    if [[ $# -gt 1 ]] ; then
+       echo 'Please, specify only one ip address!'
+       exit 1
+    fi
+    MICADO_MASTER=$1
+fi
+
+curl -d input="https://raw.githubusercontent.com/jaydesl/COLARepo/master/examples/stressng.yaml" -X POST http://$MICADO_MASTER:5050/engine/
