a6ea3cc466d281dfde508b56d3d7a950927e2567
jaydesl <jay.deslauriers@gmail.com>
2018-09-05 10:32:23 +0100

    Improve stressng test UX

--- testing/stressng/2-list-apps.sh
+++ testing/stressng/2-list-apps.sh
@@ -1,13 +1,13 @@
+#!/bin/sh
+
+settings_file="./_settings"
+
+. $settings_file
+
 if [ -z "$MICADO_MASTER" ]; then
-    if [[ $# -eq 0 ]] ; then
-       echo 'Please, specify the master ip address (or set MICADO_MASTER!)'
-       exit 1
-    fi
-    if [[ $# -gt 2 ]] ; then
-       echo 'Please, specify only one ip address!'
-       exit 1
-    fi
-    MICADO_MASTER=$2
+  echo "Please, set MICADO_MASTER in file named \"$settings_file\"!"
+  exit
 fi
 
+echo "Retrieving list of running apps from MiCADO at $MICADO_MASTER..."
 curl -X GET http://$MICADO_MASTER:5050/v1.0/list_app
