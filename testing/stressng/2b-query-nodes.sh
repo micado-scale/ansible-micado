0f252578b8b93305923a0f983acd7094ad849ae8
jaydesl <jay.deslauriers@gmail.com>
2018-09-06 10:40:42 +0100

    Permit configurable MiCADO port

--- testing/stressng/2b-query-nodes.sh
+++ testing/stressng/2b-query-nodes.sh
@@ -22,5 +22,5 @@ if [ -z "$SSL_PASS" ]; then
   echo " Please, set SSL_PASS in file named \"$settings_file\"!"
 fi
 
-echo "Deleting app with id \"$APP_ID\" from MiCADO at $MICADO_MASTER..."
-curl --insecure -X DELETE https://$SSL_USER:$SSL_PASS@$MICADO_MASTER:$MICADO_PORT/toscasubmitter/v1.0/app/undeploy/$APP_ID
+echo "Fetching nodes info for appid $APP_ID from MiCADO at $MICADO_MASTER..."
+curl --insecure -X GET https://$SSL_USER:$SSL_PASS@$MICADO_MASTER:$MICADO_PORT/toscasubmitter/v1.0/app/$APP_ID/nodes
