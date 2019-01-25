dd1064b567d4a52bf6d8c17600f107ec5c72f2f0
James Deslauriers <jay.deslauriers@gmail.com>
2019-01-25 12:38:35 +0000

    Insert Wordpress demo

---
+++ testing/wordpress/query-status.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+settings_file="./_settings"
+
+. $settings_file 
+
+if [ -z "$MICADO_MASTER" ]; then
+  echo "Please, set MICADO_MASTER in file named \"$settings_file\"!"
+  exit
+fi
+
+if [ -z "$MICADO_PORT" ]; then
+  echo "Please, set MICADO_PORT in file named \"$settings_file\"!"
+  exit
+fi
+
+if [ -z "$APP_ID" ]; then
+  echo "Please, set APP_ID in file named \"$settings_file\"!"
+  exit
+fi
+
+if [ -z "$SSL_USER" ]; then
+  echo " Please, set SSL_USER in file named \"$settings_file\"!"
+fi
+
+if [ -z "$SSL_PASS" ]; then
+  echo " Please, set SSL_PASS in file named \"$settings_file\"!"
+fi
+
+echo "Fetching status info for appid $APP_ID from MiCADO at $MICADO_MASTER..." 
+curl --insecure -s -X GET -u "$SSL_USER":"$SSL_PASS" https://$MICADO_MASTER:$MICADO_PORT/toscasubmitter/v1.0/app/$APP_ID/status | jq
